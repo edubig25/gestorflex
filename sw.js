a58fd3131f6eeef85d381b3f4de6ba2502aaf481
edubigplay <eduardofonsecasouzza@gmail.com>
2026-06-09 01:38:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+const CACHE_NAME = 'gestor-flex-logo2';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
